2be8671af6f1c26bde5ce951f48c869e03466408
KuleshovNikita <79578426+KuleshovNikita@users.noreply.github.com>
2022-10-14 14:49:20 +0300

    added auth scheme

---
+++ PetHelper.Api/Controllers/AuthenticationController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PetHelper.Domain;
+
+namespace PetHelper.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        [HttpPost("login")]
+        public void Login([FromBody] AuthModel authModel)
+        {
+            //TODO выполнить запрос к БД на аунтификацию, если успешно - перенаправить по returnUrl
+        }
+    }
+}
---
+++ PetHelper.Domain/AuthModel.cs
@@ -0,0 +1,11 @@
+﻿namespace PetHelper.Domain
+{
+    public record AuthModel
+    {
+        public string Login { get; set; }
+
+        public string Password { get; set; }
+
+        public string ReturnUrl { get; set; }
+    }
+}
