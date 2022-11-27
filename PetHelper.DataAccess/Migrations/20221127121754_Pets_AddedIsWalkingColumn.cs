1d242eab0c8076bc13b558eef071f30a5a234902
KuleshovNikita <79578426+KuleshovNikita@users.noreply.github.com>
2022-11-27 14:18:18 +0200

    added new migration

---
+++ PetHelper.DataAccess/Migrations/20221127121754_Pets_AddedIsWalkingColumn.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PetHelper.DataAccess.Migrations
+{
+    public partial class Pets_AddedIsWalkingColumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsWalking",
+                table: "Pets",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsWalking",
+                table: "Pets");
+        }
+    }
+}
